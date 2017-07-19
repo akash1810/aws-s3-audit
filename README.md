84f4f7a2935e0453e0cf730d807c4c4182566a02
Akash A <akash1810@gmail.com>
2017-07-19 13:29:44 +0100

    show if a bucket has a public acl or policy

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # S3 Audit
 
-Create a CSV detailing the creation time and name of S3 buckets.
+Create a CSV detailing the creation time, name and public access state of S3 buckets.
+
+A bucket is deemed public if:
+- its ACL allows `READ`, `READ_ACP` or `FULL_CONTROL` to all users
+- its policy allows `s3:GetObject` to the `*` Principal 
+
+See:
+- http://docs.aws.amazon.com/AmazonS3/latest/dev/acl-overview.html#permissions
+- http://docs.aws.amazon.com/AmazonS3/latest/dev/example-bucket-policies.html
 
 Install dependencies with `npm install`
 
--- s3-audit.js
+++ s3-audit.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 
 class Config {
     static get profile() {
-        return process.env.PROFILE || 'default';
+        return process.env.PROFILE || 'media-service';
     }
     static get region() {
         return process.env.REGION || 'eu-west-1';
@@ -20,34 +20,96 @@ AWS.config.update({
     region: Config.region
 });
 
-const s3 = new AWS.S3();
 
-s3.listBuckets({}, (err, data) => {
-    if (err) {
-        console.log(err);
-        throw err;
-    }
+function getBucketInfo(bucket) {
+    return new Promise((resolve, reject) => {
+        const s3 = new AWS.S3();
 
-    const buckets = data.Buckets.map(bucket => {
-        return {
-            Created: bucket.CreationDate,
-            Bucket: bucket.Name,
-            Account: Config.profile
-        };
+        s3.getBucketAcl({Bucket: bucket.Name}, (err, acl) => {
+            if (err) {
+                reject(err);
+            } else {
+                const publicAcls = acl.Grants.filter(g => {
+                    return g.Grantee.URI
+                        && g.Grantee.URI === 'http://acs.amazonaws.com/groups/global/AllUsers'
+                        && ['READ', 'READ_ACP', 'FULL_CONTROL'].includes(g.Permission);
+                });
+
+                const bucketInfo = {
+                    Created: bucket.CreationDate,
+                    Bucket: bucket.Name,
+                    Account: Config.profile,
+                    IsPublic: publicAcls.length !== 0
+                };
+
+                s3.getBucketPolicy({Bucket: bucket.Name}, (e, policyData) => {
+                    if (e) {
+                        if (e.code === 'NoSuchBucketPolicy') {
+                            resolve(bucketInfo);
+                        } else {
+                            reject(e);
+                        }
+                    } else {
+                        const policy = JSON.parse(policyData.Policy);
+
+                        const publicGetters = policy.Statement.filter(statement => {
+                            return statement.Effect === 'Allow'
+                                && statement.Action === 's3:GetObject'
+                                && statement.Principal === '*';
+                        });
+
+                        const bucketInfoWithPolicy = Object.assign({}, bucketInfo, {
+                            IsPublic: publicAcls.length !== 0 || publicGetters.length !== 0
+                        });
+
+                        resolve(bucketInfoWithPolicy);
+
+                    }
+                });
+
+            }
+        });
     });
+}
 
-    const fields = Object.keys(buckets[0]);
+function getBuckets() {
+    return new Promise((resolve, reject) => {
+        const s3 = new AWS.S3();
 
-    const csv = json2csv({data: buckets, fields: fields});
+        s3.listBuckets({}, (err, data) => {
+            if (err) {
+                reject(err);
+            }
 
-    const filename = `${Config.profile}-buckets.csv`;
+            resolve(data.Buckets);
+        });
+    });
+}
 
-    fs.writeFile(filename, csv, err => {
-        if (err) {
+getBuckets().then(buckets => {
+    const promises = buckets.map(bucket => getBucketInfo(bucket));
+
+    Promise.all(promises)
+        .then(bucketInfo => {
+            const fields = Object.keys(bucketInfo[0]);
+
+            const csv = json2csv({data: bucketInfo, fields: fields});
+
+            const filename = `${Config.profile}-buckets.csv`;
+
+            fs.writeFile(filename, csv, err => {
+                if (err) {
+                    console.log(err);
+                    throw err;
+                }
+
+                console.log(`${filename} created`);
+            });
+        }).catch(err => {
             console.log(err);
-            throw err;
-        }
-
-        console.log(`${filename} created`);
-    });
+            throw(err);
+        });
+}).catch(err => {
+    console.log(err);
+    throw err;
 });
